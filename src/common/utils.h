8da3d91617eb072d987c296b947cd4fb88c21f61
Mateusz Malinowski <malina314@op.pl>
2023-01-06 22:20:24 +0100

    Clean code

--- src/common/utils.h
+++ src/common/utils.h
@@ -6,14 +6,6 @@
 #include <type_traits>
 #include <chrono>
 
-// TODO: remove
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdarg.h>
-//#include <errno.h>
-#include <sys/types.h>
-#include <string.h>
-
 namespace utils {
     using time_point = std::chrono::system_clock::time_point;
     using duration = std::chrono::duration<float, std::micro>;
@@ -37,36 +29,6 @@ namespace utils {
             *ppInterfaceToRelease = nullptr;
         }
     }
-
-    // todo: remove
-    static inline void logerr(const char *filename, const char *funcname, int line, const char *fmt = "", ...) {
-        va_list fmt_args;
-
-        FILE *fp = fopen("logs/log.txt", "a");
-
-        fprintf(fp, "LOG in %s:%s:%d: ", filename, funcname, line);
-
-        va_start(fmt_args, fmt);
-        vfprintf(fp, fmt, fmt_args);
-        va_end(fmt_args);
-
-        fprintf(fp, "\n");
-
-        fflush(fp);
-
-        fclose(fp);
-
-//        fprintf(stderr,"ERRNO: (%d; %s)\n", errno, strerror(errno));
-    }
-
-// todo: remove
-//#define SHOULD_LOG
-#ifdef SHOULD_LOG
-#define LOG(...) utils::logerr(__FILE__, __func__, __LINE__ __VA_OPT__(,) __VA_ARGS__)
-#else
-#define LOG(...)
-#endif
-
 } // namespace utils
 
 #endif //ASTEROID_DESTROYER_UTILS_H
